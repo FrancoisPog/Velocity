614f712d68c21a68bc67a28e6a145a0b53d8dc63
FrancoisPog <francoispoguet@gmail.com>
2020-10-26 12:03:05 +0100

    JRegistre Tests completed

--- src/test/java/fr/ufc/l3info/oprog/JRegistreTest.java
+++ src/test/java/fr/ufc/l3info/oprog/JRegistreTest.java
@@ -48,9 +48,10 @@ public class JRegistreTest {
     }
 
     @Test
-    public void emprunterDejaEmprunter(){
+    public void emprunterDejaEmprunter() throws IncorrectNameException {
         assertEquals(0,registre.emprunter(abonne,velo,maintenantMoinsNMin(10)));
         assertEquals(-2,registre.emprunter(abonne,velo,maintenantMoinsNMin(0)));
+        assertEquals(-2,registre.emprunter(new Abonne("Keyser Söze"),velo,maintenantMoinsNMin(2)));
     }
 
     @Test
@@ -63,6 +64,20 @@ public class JRegistreTest {
     @Test
     public void emprunterVeloNull(){
         assertEquals(-1,registre.emprunter(abonne,null,maintenantMoinsNMin(10)));
+    }
+
+    @Test
+    public void emprunterAbonneNull(){
+            assertEquals(-1,registre.emprunter(null,velo,maintenantMoinsNMin(3)));
+    }
+
+    @Test
+    public void emprunterAvantAutreEmprunt(){
+        assertEquals(0,registre.emprunter(abonne,velo,maintenantMoinsNMin(5)));
+        assertEquals(0,registre.retourner(velo,maintenantMoinsNMin(2)));
+        assertEquals(0,registre.emprunter(abonne,velo,maintenantMoinsNMin(10)));
+        assertEquals(-3,registre.retourner(velo,maintenantMoinsNMin(3)));
+        assertEquals(0,registre.retourner(velo,maintenantMoinsNMin(0)));
     }
 
     @Test
@@ -80,7 +95,7 @@ public class JRegistreTest {
     }
 
     @Test
-    public void retournerNomEmprunter(){
+    public void retournerNonEmprunte(){
         assertEquals(-2,registre.retourner(velo,maintenantMoinsNMin(0)));
     }
 
@@ -103,10 +118,10 @@ public class JRegistreTest {
     @Test
     public void nbEmpruntEnCours(){
         assertEquals(0,registre.emprunter(abonne,velo,maintenantMoinsNMin(10)));
-        assertEquals(-2,registre.emprunter(abonne,velo,maintenantMoinsNMin(10)));
+        assertEquals(-2,registre.emprunter(abonne,velo,maintenantMoinsNMin(9)));
 
-        assertEquals(0,registre.emprunter(abonne,new Velo('h'),maintenantMoinsNMin(10)));
-        assertEquals(0,registre.emprunter(abonne,new Velo('f'),maintenantMoinsNMin(10)));
+        assertEquals(0,registre.emprunter(abonne,new Velo('h'),maintenantMoinsNMin(8)));
+        assertEquals(0,registre.emprunter(abonne,new Velo('f'),maintenantMoinsNMin(7)));
 
         assertEquals(3,registre.nbEmpruntsEnCours(abonne));
     }
@@ -114,6 +129,75 @@ public class JRegistreTest {
     @Test
     public void nbEmpruntPlusieursAbonne() throws IncorrectNameException {
         Abonne abonne1 = new Abonne("Jack");
+
+        assertEquals(0,registre.emprunter(abonne,velo,maintenantMoinsNMin(10)));
+        assertEquals(0,registre.emprunter(abonne1,new Velo('h'),maintenantMoinsNMin(9)));
+        assertEquals(0,registre.emprunter(abonne,new Velo('f'),maintenantMoinsNMin(8)));
+
+        assertEquals(2,registre.nbEmpruntsEnCours(abonne));
+        assertEquals(1,registre.nbEmpruntsEnCours(abonne1));
     }
+
+    @Test
+    public void facturationUnEmprunt(){
+        assertEquals(0,registre.emprunter(abonne,velo,maintenantMoinsNMin(180)));
+        assertEquals(0,registre.retourner(velo,maintenantMoinsNMin(60)));
+
+        assertEquals(4.0,registre.facturation(abonne,maintenantMoinsNMin(200),maintenantMoinsNMin(0)),1e-3);
+        assertEquals(0,registre.facturation(abonne,maintenantMoinsNMin(200),maintenantMoinsNMin(70)),1e-3);
+    }
+
+    @Test
+    public void facturationAvantEmprunt(){
+        assertEquals(0,registre.emprunter(abonne,velo,maintenantMoinsNMin(180)));
+        assertEquals(0,registre.retourner(velo,maintenantMoinsNMin(60)));
+
+        assertEquals(0,registre.facturation(abonne,maintenantMoinsNMin(300),maintenantMoinsNMin(200)),1e-3);
+    }
+
+    @Test
+    public void facturationPlusieursEmprunt(){
+        assertEquals(0,registre.emprunter(abonne,velo,maintenantMoinsNMin(180)));
+        assertEquals(0,registre.retourner(velo,maintenantMoinsNMin(60)));
+
+        assertEquals(0,registre.emprunter(abonne,velo,maintenantMoinsNMin(40)));
+        assertEquals(0,registre.retourner(velo,maintenantMoinsNMin(10)));
+
+        assertEquals(0,registre.emprunter(abonne,velo,maintenantMoinsNMin(250)));
+        assertEquals(0,registre.retourner(velo,maintenantMoinsNMin(235)));
+
+        assertEquals(5.5,registre.facturation(abonne,maintenantMoinsNMin(500),maintenantMoinsNMin(0)),1e-3);
+        assertEquals(0.5,registre.facturation(abonne,maintenantMoinsNMin(300),maintenantMoinsNMin(200)),1e-3);
+        assertEquals(5,registre.facturation(abonne,maintenantMoinsNMin(100),maintenantMoinsNMin(0)),1e-3);
+        assertEquals(0,registre.facturation(abonne,maintenantMoinsNMin(-10),maintenantMoinsNMin(-100)),1e-3);
+    }
+
+    @Test
+    public void facturationPrecisMinute(){
+        assertEquals(0,registre.emprunter(abonne,velo,maintenantMoinsNMin(180)));
+        assertEquals(0,registre.retourner(velo,maintenantMoinsNMin(114)));
+
+        assertEquals(2.2,registre.facturation(abonne,maintenantMoinsNMin(180),maintenantMoinsNMin(114)),1e-3);
+    }
+
+    @Test
+    public void facturationEmpruntSimultane(){
+        assertEquals(0,registre.emprunter(abonne,velo,maintenantMoinsNMin(180)));
+        assertEquals(0,registre.retourner(velo,maintenantMoinsNMin(60)));
+
+        IVelo deuxieme = new Velo('f');
+        assertEquals(0,registre.emprunter(abonne,deuxieme,maintenantMoinsNMin(150)));
+        assertEquals(0,registre.retourner(deuxieme,maintenantMoinsNMin(70)));
+
+        IVelo troisieme = new Velo('f');
+        assertEquals(0,registre.emprunter(abonne,troisieme,maintenantMoinsNMin(170)));
+        assertEquals(0,registre.retourner(troisieme,maintenantMoinsNMin(30)));
+
+        assertEquals(11.33,registre.facturation(abonne,maintenantMoinsNMin(180),maintenantMoinsNMin(0)),1e-2);
+        assertEquals(6.67,registre.facturation(abonne,maintenantMoinsNMin(160),maintenantMoinsNMin(40)),1e-2);
+
+    }
+
+
 
 }
