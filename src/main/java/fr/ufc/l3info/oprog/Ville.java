bfbab326e11cf6f7fbc0a5425e9ba005e6f34646
Valentin Perignon <valentin.perignon@orange.fr>
2020-11-26 10:04:39 +0100

    OKAY I WAS KIDING YOU, here is Ville

--- src/main/java/fr/ufc/l3info/oprog/Ville.java
+++ src/main/java/fr/ufc/l3info/oprog/Ville.java
@@ -3,12 +3,20 @@ package fr.ufc.l3info.oprog;
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class Ville implements Iterable<Station> {
-    private Station[] stations;
-    private Abonne[] abonnes;
+    private List<Station> stations;
+    private List<Abonne> abonnes;
     private IRegistre reg;
+
+    public Ville() {
+        this.stations = new LinkedList<>();
+        this.abonnes = new LinkedList<>();
+        this.reg = new JRegistre();
+    }
 
     public void initialiser(File f) throws IOException {
 
