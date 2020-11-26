c6b5e904159e28d6766092cc8853f351b030e020
François Poguet <francoispoguet@gmail.com>
2020-11-26 10:02:28 +0000

    Classes Iterateur et Exploitant implémentées, non testées

--- src/main/java/fr/ufc/l3info/oprog/ClosestStationIterator.java
+++ src/main/java/fr/ufc/l3info/oprog/ClosestStationIterator.java
@@ -1,23 +1,46 @@
 package fr.ufc.l3info.oprog;
 
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 public class ClosestStationIterator implements Iterator<Station> {
+    private final Set<Station> stations;
+    private Station current;
 
-    ClosestStationIterator(Set<Station> stations, Station s){}
+    ClosestStationIterator(Set<Station> stations, Station s){
+        this.stations = stations;
+        this.current = s;
+    }
 
 
     @Override
     public boolean hasNext() {
-        return false;
+        return this.stations.size() == 0;
     }
 
     @Override
     public Station next() {
-        return null;
+        Station res = this.current;
+        // Calcul de la station la plus proche de la station courante
+        Station closest = null;
+        double min = 0;
+        for(Station s : this.stations){
+            double distance = s.distance(this.current);
+            if(closest == null || distance < min){
+                min = distance;
+                closest = s;
+            }
+        }
+        // Suppression de la station courante des stations restantes
+        this.stations.remove(this.current);
+        // La prochaine station devient la plus proche de la station courante
+        this.current = closest;
+
+        // On retourne l'ancienne station courante
+        return res;
     }
 
     @Override
     public void remove() {}
+
+
 }
