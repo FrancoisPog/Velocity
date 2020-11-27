276138c1021af4a13a73f34c50bf00374ba43f4e
Valentin Perignon <valentin.perignon@orange.fr>
2020-11-27 20:08:40 +0100

    WIP exploitant tests

--- src/main/java/fr/ufc/l3info/oprog/ClosestStationIterator.java
+++ src/main/java/fr/ufc/l3info/oprog/ClosestStationIterator.java
@@ -11,7 +11,6 @@ public class ClosestStationIterator implements Iterator<Station> {
         this.current = s;
     }
 
-
     @Override
     public boolean hasNext() {
         return this.stations.size() != 0;
@@ -20,19 +19,21 @@ public class ClosestStationIterator implements Iterator<Station> {
     @Override
     public Station next() {
         Station res = this.current;
+
+        // Suppression de la station courante des stations restantes
+        this.stations.remove(this.current);
+
         // Calcul de la station la plus proche de la station courante
         Station closest = null;
         double min = 0;
         for(Station s : this.stations) {
-            if (s == this.current) continue;
             double distance = s.distance(this.current);
             if(closest == null || distance < min){
                 min = distance;
                 closest = s;
             }
         }
-        // Suppression de la station courante des stations restantes
-        this.stations.remove(this.current);
+
         // La prochaine station devient la plus proche de la station courante
         this.current = closest;
 
